352c119e196b8b4d6dcefa7b2270f720c734e898
Ahmed <amartazanov892@gmail.com>
2024-12-04 19:59:25 +0300

    master commit 1.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
 import Registration from "./pages/registration/Registration"
 
 function App() {
   return (
     <div className="App">
-      <Registration />
+      <Routes>
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/" />
+      </Routes>
     </div>
   );
 }
