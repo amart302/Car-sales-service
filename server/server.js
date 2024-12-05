27666ec7938053b12d870a855d6b6d1168a0dff4
amart <amartazanov892@gmail.com>
2024-12-05 11:04:22 +0300

    master commit 3

--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const cors = require("cors");
 const port = 8000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/api/registration", (req, res) => {
-    const data = req.data;
+    const data = req.body;
     console.log(data);
 
     res.status(201).json({ message: "Данные получены" });
